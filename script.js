f7fcc64a4a052ff9d78fd59b044abec97a375016
Yevhen-Danylchenko <103068879+Yevhen-Danylchenko@users.noreply.github.com>
2024-09-24 20:24:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// class Cars{
+//     constructor(firm,model,year,speed){
+//         this.firm = firm;
+//         this.model = model;
+//         this.year = year;
+//         this.speed =speed;
+//     }
+
+//     getTime(s) {
+//         let time = s / this.speed;
+//         let tmp = Math.floor(time / 4);
+//         return Math.floor(time += tmp);
+//     }
+
+//     getInfo() {
+//         console.log(`Firm: ${this.firm} \n Model: ${this.model} \n Year: ${this.year} \n Speed: ${this.speed}`);
+//     }
+// }
+
+// const Audi = new Cars('Japan', 'A4', '2022', 190);
+
+// console.log(Audi.getTime(2000));
+// Audi.getInfo();
+
+
+
+
+
+
