fa74ea127c9edd40adb5097bb9796d1bb206e11f
Yevhen-Danylchenko <103068879+Yevhen-Danylchenko@users.noreply.github.com>
2024-09-28 13:24:50 +0300

    Home work

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ========================= Завдання 1 =============================
+
 // class Cars{
 //     constructor(firm,model,year,speed){
 //         this.firm = firm;
@@ -21,6 +23,84 @@
 
 // console.log(Audi.getTime(2000));
 // Audi.getInfo();
+
+
+// ============================ Завдання 2 =================================
+
+let Nod = [];
+let numer;
+let denom;
+let num;
+let num1;
+
+class Fractions {
+    constructor(numerator, denominator, numerator1, denominator1) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+        this.numerator1 = numerator1;
+        this.denominator1 = denominator1;
+    }
+
+    getNod() {
+        for (let i = 2; i <= this.denominator; i++) {
+            if (this.denominator % i === 0) {
+                Nod.push(i);
+            }
+        }
+
+        for (let i = 2; i <= this.denominator1; i++) {
+            if (this.denominator1 % i === 0) {
+                Nod.push(i);
+            }
+        }
+
+        const Nod1 = (...Nod) => {
+            num = Nod.reduce((total, arr) => total * arr, 0)
+        }
+
+        num = Nod1;
+
+        num1 = num;
+
+        num = num / this.denominator;
+        num1 = num1 / this.denominator1;
+
+        this.numerator *= num;
+        this.denominator *= num;
+        this.numerator1 *= num1;
+        this.denominator1 *= num1;
+    }
+
+    getSum() {
+        numer = this.numerator + this.numerator1;
+        denom = this.denominator;
+
+        console.log(numer, denom);
+    }
+
+    getSubstraction(){
+        numer = this.numerator - this.numerator1;
+        denom = this.denominator;
+        console.log(numer, denom);
+    }
+
+    getMultiplication(){
+        numer = this.numerator * this.numerator1;
+        denom = this.denominator * this.denominator1;
+        console.log(numer, denom);
+    }
+
+    getDivision(){
+        numer = this.numerator * this.denominator1;
+        denom = this.denominator * this.numerator1;
+        console.log(numer, denom);
+    }
+}
+
+const numb = new Fractions(2,3,1,2);
+
+numb.getNod();
+numb.getSum();
 
 
 
